7012e84a86f5e7a006033f749ff01720e51225c5
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-03-07 16:15:19 +0100

    add message ita in formReuqst Poject

--- app/Http/Requests/UpdateProjectRequest.php
+++ app/Http/Requests/UpdateProjectRequest.php
@@ -31,4 +31,21 @@ class UpdateProjectRequest extends FormRequest
             'total_hours' => 'nullable|numeric|max:999',
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Il campo nome è obbligatorio.',
+            'name.max' => 'Il campo nome non può superare i 255 caratteri.',
+            'thumb.url' => 'Il campo thumb deve essere un URL valido.',
+            'description.max' => 'Il campo descrizione non può superare i 5000 caratteri.',
+            'technologies.required' => 'Il campo tecnologie è obbligatorio.',
+            'technologies.max' => 'Il campo tecnologie non può superare i 255 caratteri.',
+            'start_date.required' => 'Il campo data di inizio è obbligatorio.',
+            'start_date.date' => 'Il campo data di inizio deve essere una data valida.',
+            'last_update_date.date' => 'Il campo data ultimo aggiornamento deve essere una data valida.',
+            'total_hours.numeric' => 'Il campo ore totali deve essere un numero.',
+            'total_hours.max' => 'Il campo ore totali non può superare 999.',
+        ];
+    }
 }
